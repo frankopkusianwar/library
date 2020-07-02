26e73e5a7c249dd9640098b2be76297653fcd3c8
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-02 14:46:51 +0530

    Add render function to index.js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log("we made it")
+const table = document.querySelector('tbody');
+
+let myLibrary = [
+  {
+    title: 'Harry potter',
+    author: 'J.k. Rolings',
+    pages: 300,
+    read: 'Completed',
+  },
+  {
+    title: 'Harry potter',
+    author: 'J.k. Rolings',
+    pages: 300,
+    read: 'Completed',
+  },
+  {
+    title: 'Harry potter',
+    author: 'J.k. Rolings',
+    pages: 300,
+    read: 'Completed',
+  }, {
+    title: 'Harry potter',
+    author: 'J.k. Rolings',
+    pages: 300,
+    read: 'Completed',
+  }
+];
+
+
+function render() {
+
+  myLibrary.forEach((book, index) => {
+    const tableRow = document.createElement('tr');
+    const rowIndex = document.createElement('th');
+    const rowTitle = document.createElement('td');
+    const rowAuthor = document.createElement('td');
+    const rowPages = document.createElement('td');
+    const RowRead = document.createElement('td');
+
+    rowIndex.textContent = (index + 2);
+    rowTitle.textContent = book.title;
+    rowAuthor.textContent = book.author;
+    rowPages.textContent = book.pages;
+    RowRead.textContent = book.read;
+
+    var newRow = table.appendChild(tableRow);
+    newRow.append(rowIndex);
+    newRow.append(rowTitle);
+    newRow.append(rowAuthor);
+    newRow.append(rowPages);
+    newRow.append(RowRead);
+  });
+
+}
+
+render();
