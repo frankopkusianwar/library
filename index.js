b8619f47b383cc3b1ef4916f0be20eb6e7810793
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-02 23:41:13 +0530

    Fix linter errors

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const table = document.querySelector('tbody');
 const openFormButton = document.querySelector('#open');
 const form = document.querySelector('#form');
-const deleteBookButton = document.querySelector('td button');
-const formToggle = document.querySelector('#center-form')
+const formToggle = document.querySelector('#center-form');
 
-let myLibrary = [];
+const myLibrary = [];
 
 function render() {
   if (myLibrary.length) {
-    book = myLibrary[myLibrary.length -1];
-    index = myLibrary.length;
+    const book = myLibrary[myLibrary.length - 1];
+    const index = myLibrary.length;
     const tableRow = document.createElement('tr');
     const rowIndex = document.createElement('th');
     const rowTitle = document.createElement('td');
@@ -18,9 +17,7 @@ function render() {
     const rowRead = document.createElement('td');
     const rowDelete = document.createElement('td');
     const rowButton = document.createElement('button');
-    rowButton.className = "delete";
     const rowReadButton = document.createElement('button');
-    rowReadButton.className = "read";
 
     rowIndex.textContent = (index + 1);
     rowTitle.textContent = book.title;
@@ -28,8 +25,10 @@ function render() {
     rowPages.textContent = book.pages;
     rowButton.textContent = 'Delete';
     rowReadButton.textContent = book.read;
+    rowReadButton.className = 'read';
+    rowButton.className = 'delete';
 
-    var newRow = table.appendChild(tableRow);
+    const newRow = table.appendChild(tableRow);
     newRow.append(rowIndex);
     newRow.append(rowTitle);
     newRow.append(rowAuthor);
@@ -39,12 +38,11 @@ function render() {
     rowDelete.append(rowButton);
     newRow.append(rowDelete);
   }
-
 }
 
 function toggle() {
-  formToggle.classList.toggle("form-toggle");
-};
+  formToggle.classList.toggle('form-toggle');
+}
 
 function openForm() {
   openFormButton.addEventListener('click', toggle);
@@ -56,49 +54,50 @@ function checked(check) {
   return check ? 'Completed' : 'Yet To Read';
 }
 
-form.addEventListener("submit", function(e) {
+function deleteBook(e) {
+  e.target.parentNode.parentNode.remove();
+}
+
+function deleteBookRow() {
+  document.querySelectorAll('.delete').forEach((button) => {
+    button.addEventListener('click', deleteBook);
+  });
+}
+
+function readStatus(e) {
+  e.target.textContent === 'Completed' ? e.target.textContent = 'Yet To Read' : e.target.textContent = 'Completed'; // eslint-disable-line no-unused-expressions
+}
+
+function readBookStatus() {
+  document.querySelectorAll('.read').forEach((button) => {
+    button.addEventListener('click', readStatus);
+  });
+}
+
+function addBookToLibrary(e) {
   e.preventDefault();
-  if (e.target.title.value.length > 1 && e.target.author.value.length > 1 && e.target.pages.value.length > 1) {
+  if (e.target.title.value.length > 1 && e.target.author.value.length > 1
+    && e.target.pages.value.length > 1) {
     myLibrary.push(
       {
         title: e.target.title.value,
         author: e.target.author.value,
         pages: e.target.pages.value,
         read: checked(e.target.readStatus.checked),
-      }
-    )
+      },
+    );
 
     render();
     e.target.title.value = '';
     e.target.author.value = '';
     e.target.pages.value = '';
+    e.target.readStatus.checked = '';
     deleteBookRow();
     readBookStatus();
-    toggle()
+    toggle();
+  } else {
+    alert('Please fill all the fields'); // eslint-disable-line no-alert
   }
-  else {
-    alert('Please fill all the fields')
-  }
-});
-
-function deleteBookRow() {
-  document.querySelectorAll('.delete').forEach((button) => {
-    button.addEventListener('click', deleteBook)
-  })
 }
 
-function deleteBook(e) {
-  e.target.parentNode.parentNode.remove();
-}
-
-function readBookStatus() {
-  document.querySelectorAll('.read').forEach((button) => {
-    button.addEventListener('click', readStatus)
-  })
-}
-
-function readStatus(e) {
-  e.target.textContent === "Complete" ? e.target.textContent = "Yet To Read" : e.target.textContent = "Complete";
-}
-  
-  
+form.addEventListener('submit', addBookToLibrary);
